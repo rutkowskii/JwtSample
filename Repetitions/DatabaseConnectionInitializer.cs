9de5cfb76539f3c71054fedc87a962ce93fd45b1
Piotrz <ruciof300@o2.pl>
2018-02-13 00:26:13 +0000

    Initial commit

---
+++ Repetitions/DatabaseConnectionInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace Repetitions
+{
+    public class DatabaseConnectionInitializer
+    {
+        public void Start()
+        {
+            DbConfiguration.SetConfiguration(new DefaultDbConfig());
+            using (var context = new SamplesContext())
+            {
+                var xx = context.Players.ToArray();
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Repetitions/DefaultDbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Npgsql;
+
+namespace Repetitions
+{
+    public class DefaultDbConfig : DbConfiguration
+    {
+        public DefaultDbConfig()
+        {
+            SetProviderServices("Npgsql", NpgsqlServices.Instance);
+            SetProviderFactory("Npgsql", NpgsqlFactory.Instance);
+            SetDefaultConnectionFactory(new NpgsqlConnectionFactory());
+        }
+    }
+}
